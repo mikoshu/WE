99b4382c3ce0e60079b6aa11094a7fe9ee01fb70
ronhng <ronhng@qq.com>
2016-05-03 12:08:27 +0800

    wrap jquery

--- js/comm.js
+++ js/comm.js
@@ -1,31 +1,33 @@
-// 通用
-$(document).ready(function(){ // 导航栏动画
-	var $nav = $(".top-nav");
-	$(window,document).scroll(function(){
-		var top = $(document).scrollTop()
-		if(top > 0){
-			$nav.addClass("not-top")
-		}else{
-			$nav.removeClass("not-top")
-		}
-	})
-	$('.ani-show').detectAndAddClass('ani-in')
-})
-
-
-$.fn.detectAndAddClass = function(className) { // 监听页面滚动是否到达元素高度
-	this.each(function(){
-		var $this = $(this),
-			top = $this.offset().top,
-			w_h = window.innerHeight,
-			height = w_h - ($this.height()*0.2);
-		function regScroll() {
-			// detect height
-			if($(document).scrollTop() >= top-height ) {
-				$this.addClass(className)
-				$(window).off('scroll', regScroll)
+!function($) {
+	// 通用
+	$(document).ready(function(){ // 导航栏动画
+		var $nav = $(".top-nav");
+		$(window,document).scroll(function(){
+			var top = $(document).scrollTop()
+			if(top > 0){
+				$nav.addClass("not-top")
+			}else{
+				$nav.removeClass("not-top")
 			}
-		}
-		$(window).on('scroll', regScroll)
+		})
+		$('.ani-show').detectAndAddClass('ani-in')
 	})
-}
+
+
+	$.fn.detectAndAddClass = function(className) { // 监听页面滚动是否到达元素高度
+		this.each(function(){
+			var $this = $(this),
+				top = $this.offset().top,
+				w_h = window.innerHeight,
+				height = w_h - ($this.height()*0.2);
+			function regScroll() {
+				// detect height
+				if($(document).scrollTop() >= top-height ) {
+					$this.addClass(className)
+					$(window).off('scroll', regScroll)
+				}
+			}
+			$(window).on('scroll', regScroll)
+		})
+	}
+}(jQuery)
